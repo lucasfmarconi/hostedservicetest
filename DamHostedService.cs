e271fdf94c8e6482b47fac12861684ac8f76d320
Lucas Marçoni <lucasfmarconi@gmail.com>
2019-05-25 23:51:26 -0300

    Adding project

---
+++ DamHostedService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hostedservicetest
+{
+    public class DamHostedService : IHostedService
+    {
+        private Timer _timer;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(HelloWorld, null, 0, 10000);
+            return Task.CompletedTask;
+        }
+
+        void HelloWorld(object state)
+        {
+            Debug.WriteLine("Hello World!");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            //New Timer does not have a stop. 
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+    }
+}
